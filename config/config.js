92ba4d8876304d5de101b209f5617f5361adfc86
Cesar Herrera <cesarherreras@live.com>
2022-09-04 19:07:05 -0500

    urlDb config

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const config = {
   env: process.env.NODE_ENV || 'dev',
   isProd: process.env.NODE_ENV === 'production',
   port: process.env.PORT || 3000,
-  dbUrl: process.env.DATABASE_URL,
+  dbUrl: process.env.JAWSDB_URL,
   jwtSecret: process.env.JWT_SECRET
 }
 
